35a01bf80f42f0ee16cbe698083eb97da8631d54
Javier Andrés Luis González <javierandresluisgonzalez@gmail.com>
2024-10-27 12:46:58 -0500

    fix(main): corregir importacion de ContextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { ContextProvider } from "./context/globalContext.jsx"
+import ContextProvider from "./context/globalContext.jsx"
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
