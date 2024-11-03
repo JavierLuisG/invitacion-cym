537efddfe8d0232ce1ad108acd86674a38282edd
Javier Andrés Luis González <javierandresluisgonzalez@gmail.com>
2024-11-03 13:43:22 -0500

    feat: agregar not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import { routes } from "./utils/routes";
 import { Home } from "./routes/Home";
+import NotFound from "./routes/NotFound";
+
 
 function App() {
   
@@ -8,6 +10,7 @@ function App() {
     <>
       <Routes>
         <Route path={`${routes.home}/:name`} element={<Home />} />
+        <Route path={`${routes.notFound}`} element={<NotFound />} />
       </Routes>
     </>
   )
---
+++ src/routes/NotFound.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="container_not_found flex_column box">
+      <div className="content_text_not_found flex_column box">
+        <h1>Upss!!</h1>
+        <h2>La puerta hacia el amor no está aquí</h2>
+        <div>
+          <p>Como testigo de nuestro amor intenta de nuevo,</p>
+          <p>cada invitado es importante para nosotros.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
